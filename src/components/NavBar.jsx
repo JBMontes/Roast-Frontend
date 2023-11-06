942a3d1702de1f24d1275228ffcb4fa90efd57fa
Jeremy Montes <jeremymontes@pursuit.org>
2023-11-06 11:30:42 -0500

    created navbar component

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,11 @@
+export default function NavBar(){
+return(
+    <div className="navbar">
+    <h1>Roast</h1>
+
+    <button>New Coffee</button>
+
+    </div>
+)
+
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom"
+
+export default function Home(){
+
+return(
+    <>
+    <h1>Welcome To Roast</h1>
+    
+    </>
+)
+
+}
