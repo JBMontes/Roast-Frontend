e0b02eac26ff351985453a2b460c60ad75cd52c0
Jeremy Montes <jeremymontes@pursuit.org>
2023-11-07 01:34:41 -0500

    Completed About Page with css

---
+++ src/components/AboutCard.jsx
@@ -0,0 +1,27 @@
+import "../styles/About.css"
+
+import github from "../assets/github-mark.svg"
+
+export default function AboutCard({engineers}){
+
+return(
+    <>
+  
+        return (
+            
+            <div className="profileCard">
+
+            <h3>{engineers.name}</h3>
+            <img className='profilePic' src={engineers.pic} alt={engineers.name} />
+            <h3>{engineers.hometown}</h3>
+            <a href={engineers.github}>
+            <img className="github" src={github} />
+            </a>
+            </div>
+           
+
+        )
+   
+    </>
+)
+}
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,11 +1,20 @@
 import "../styles/About.css"
+import team from "../team.json"
+import AboutCard from "../components/AboutCard"
+
 export default function About(){
 
     return(
         <div className="about">
-           
+            <h3>Meet The Creators</h3>
+           <div className="profileList">
+            {team.map((engineers)=>{
+                return(
+                    <AboutCard engineers={engineers} />
 
-
+                )
+            })}
+        </div>
 
         </div>
     )
