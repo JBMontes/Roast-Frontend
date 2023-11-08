2fd58f270ab03384dae72959750aeb643ed35f2a
Jeremy Montes <jeremymontes@pursuit.org>
2023-11-07 20:00:15 -0500

    attempting to fix scroll issue in list component

--- src/components/FavCount.jsx
+++ src/components/FavCount.jsx
@@ -1,34 +1,32 @@
 
-import { useEffect,useState } from "react";
+import { useEffect, useState } from "react";
 import '../styles/FavCount.css'
 
-export default function FavCount(){
-    
- let sum =[]
-    const [coffees,setCoffees] = useState([]);
+export default function FavCount() {
+
+    let sum = []
+    const [coffees, setCoffees] = useState([]);
 
     const API = import.meta.env.VITE_API_URL;
 
     useEffect(() => {
         fetch(`${API}/coffees`)
-          .then((response) => response.json())
-          .then((responseJSON) => setCoffees(responseJSON))
-          .catch((error) => console.error(error));
-      });
+            .then((response) => response.json())
+            .then((responseJSON) => setCoffees(responseJSON))
+            .catch((error) => console.error(error));
+    });
 
-for(let fav of coffees){
-    sum.push(fav.is_favorite === true ? true : null)
-}
-let fav = sum.filter((outcome)=> outcome === true)
-
-let total = fav.length
-
-      return(
+    for (let fav of coffees) {
+        sum.push(fav.is_favorite === true ? true : null)
+    }
+    let fav = sum.filter((outcome) => outcome === true)
+    let total = fav.length
+    return (
         <div className="favTotal">
 
-       <h2>⭐️☕️ : {total}</h2> 
+            <h2>⭐️☕️ : {total}</h2>
 
         </div>
 
-      )
+    )
 }
